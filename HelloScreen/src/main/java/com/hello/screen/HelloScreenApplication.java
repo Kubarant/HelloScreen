7c4147a9a0f73bf946ff71359d009dee60420985
Kubarant <kiba50@gazeta.pl>
2018-03-24 17:50:36 +0100

    initial

---
+++ HelloScreen/src/main/java/com/hello/screen/HelloScreenApplication.java
@@ -0,0 +1,12 @@
+package com.hello.screen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloScreenApplication.class, args);
+	}
+}

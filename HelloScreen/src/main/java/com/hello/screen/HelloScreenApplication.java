478919c5670f25e4edd1662ea069e75d77257388
Kubarant <kiba50@gazeta.pl>
2018-03-29 16:43:29 +0200

    forgot to add in intiall commit

--- HelloScreen/src/main/java/com/hello/screen/HelloScreenApplication.java
+++ HelloScreen/src/main/java/com/hello/screen/HelloScreenApplication.java
@@ -2,11 +2,27 @@ package com.hello.screen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class HelloScreenApplication {
-
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelloScreenApplication.class, args);
+	
 	}
+	
+	
+	  @Bean
+	  public MongoClient mongoClient() {
+	    return MongoClients.create();
+	  }
+	
+	
 }

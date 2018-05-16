3c16c0e1374c9a26412465235378a2d175398dc0
Kubarant <kiba50@gazeta.pl>
2018-05-16 20:18:15 +0200

    changing location of profile resources

--- HelloScreen/src/main/resources/static/profileWebsite/slider.js
+++ HelloScreen/src/main/resources/static/profileWebsite/slider.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     let slidersHC = document.getElementsByClassName("slider");
     var sliders = Array.from(slidersHC);
-    sliders.forEach(slider => slider.value = 25);
+//    sliders.forEach(slider => slider.value = 25);
+    setInitialSlidersValues("cat-chooser");
     sliders.forEach(el => updateLabels(el));
     sliders.forEach(slider => slider.onchange = function() {
 
@@ -41,13 +42,26 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+function setInitialSlidersValues(sliderMainGroupId){
+	let sliders =getSliders(sliderMainGroupId);
+	let slidersLength = sliders.length;
+	let initialValue = slidersLength > 0? 100/slidersLength : 100;
+	sliders.forEach(slider=> slider.value = initialValue);
+	let checkValueSum = slidersLength * Math.floor(initialValue);
+	console.log(checkValueSum);
+
+	if(checkValueSum < 100 && slidersLength > 0){
+		let missingValue = 100 - checkValueSum;
+		sliders[0].value =Number.parseInt(sliders[0].value) + missingValue; 
+	}
+
+}
+
 
 function updateLabels(slider) {
 
     let childs = getAllElementsFromSameLevel(slider);
-    //	console.log(childs);
     let labels = findElementsContainingClass(childs, "slider-label");
-    //	console.log(labels);
     labels.forEach(el => el.value = slider.value);
 }
 
@@ -100,11 +114,20 @@ function getSlidersData(slidersid) {
     return svalues;
 
 }
+function getSliders(slidersid) {
+	let sliders = document.getElementById(slidersid);
+    let slidersGroup = findElementsContainingClass(Array.from(sliders.childNodes), "slider-group");
+    let sld = slidersGroup.map(sgroup => Array.from(sgroup.childNodes)).map(el=>findElementsContainingClass(el, "slider")[0]);
+    return sld;
+}
 
 function extractSliderData(slidersGroup) {
     let childs = Array.from(slidersGroup.childNodes);
-    let label = findElementsContainingClass(childs, "slider-category")[0].innerText;
-    let value = findElementsContainingClass(childs, "slider")[0].value;
+    //let label = findElementsContainingClass(childs, "slider-category")[0].innerText;
+    //let value = findElementsContainingClass(childs, "slider")[0].value;
+    let slider =findElementsContainingClass(childs, "slider")[0];
+    let value= slider.value;
+    let label = slider.name;
     let sliderData = {
         label: label,
         value: value

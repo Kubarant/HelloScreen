d6696e64833c9d0eceea8a3644f975f922608e36
Kubarant <kiba50@gazeta.pl>
2018-05-17 18:34:43 +0200

    Util to remove few specified HTML special characters like '&quote' from string

---
+++ HelloScreen/src/main/java/com/hello/screen/utils/HtmlCharsRemover.java
@@ -0,0 +1,14 @@
+package com.hello.screen.utils;
+
+public class HtmlCharsRemover {
+	public static final String HTML_CHARS_REPLACE_REGEX="&quot;|&amp;|&lt;|&gt;|&deg;|&frasl;|&oacute;";
+	
+	/**
+	 * remove SPECIFIED html special characters and entities from given string
+	 *  the regex used to replace is stored in constant "HTML_CHARS_REPLACE_REGEX"
+	 * @return string with HTML special chars replaced as spaces
+	 */
+	public static String removeSpecials(String dirtyText) {
+		return dirtyText.replaceAll(HTML_CHARS_REPLACE_REGEX, " ");
+	}
+}

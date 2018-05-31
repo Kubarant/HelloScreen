9a3444e89c19e71417963f8d288ecda5f3010747
Kubarant <kiba50@gazeta.pl>
2018-05-31 15:04:04 +0200

    Add secure sublist method to ListUtil class

--- HelloScreen/src/main/java/com/hello/screen/utils/ListUtil.java
+++ HelloScreen/src/main/java/com/hello/screen/utils/ListUtil.java
@@ -1,13 +1,19 @@
 package com.hello.screen.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListUtil {
 
-	
-	public static <T> List<T> combine(List<T> list1, List<T> list2){
-		list1.addAll(list2);
-		return list1;
-	} 
-	
+
+    public static <T> List<T> combine(List<T> list1, List<T> list2) {
+        List<T> res = new ArrayList<>(list1);
+        res.addAll(list2);
+        return res;
+    }
+
+    public static <T> List<T> sublist(List<T> list, int start, int end) {
+        int endIndex = list.size() < end ? list.size() - 1 : end;
+        return list.subList(start, endIndex);
+    }
 }
